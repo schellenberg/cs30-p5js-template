906e623b3c329090137c63b155e8b215f2e79f24
Dan Schellenberg <schellenberg@gmail.com>
2018-10-05 10:35:27 -0600

    updated p5 library to version 7.2

--- sketch.js
+++ sketch.js
@@ -5,11 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  
 }
